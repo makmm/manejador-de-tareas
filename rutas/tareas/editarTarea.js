025b0e0d0e6f01e11509a4c352ce4597fb5a46a4
makmm <matiartopoulos@gmail.com>
2017-08-23 07:46:19 -0300

    Mantener ID al editar

--- rutas/tareas/editarTarea.js
+++ rutas/tareas/editarTarea.js
@@ -4,9 +4,11 @@ module.exports = async (app) => {
   let db = await require('../../utils/db.js')()
 
   app.patch('/editarTarea', async (req, res) => {
-    delete req.body._id
-
     try {
+      // Convertir ids de las cosas a ObjectID
+      // para que funcione bien
+      req.body._id = ObjectID(req.body._id)
+
       // Reemplazar la tarea con la nueva, editada (despues voy a hacer que solo cambie lo que se cambió)
       await db.collection('tareas').replaceOne({
         _id: ObjectID(req.body._id)
