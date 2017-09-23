2fe49ae8fbd9881fe3982c158e0b1edbcbade5aa
makmm <matiartopoulos@gmail.com>
2017-09-23 17:48:44 -0300

    limpiar codigo

--- public/servicios/login.servicio.js
+++ public/servicios/login.servicio.js
@@ -3,44 +3,38 @@ app.service('loginServicio', function($http, perfilServicio){
 
   loginServicio.logeado = false
 
-  loginServicio.logearse = (usuario, contrasena) => {
+  loginServicio.logearse = (usuario, contrasena) =>
     $http.post('/logeo/sesion', {
       usuario: usuario,
       contrasena: contrasena
     })
-      .then(function successCallback(respuesta){
+      .then((respuesta) => {
         loginServicio.logeado = true
         perfilServicio.actualizarPerfil()
-      }, function errorCallback(respuesta){
-        return respuesta
       })
-  }
 
   loginServicio.checkearLogeado = () =>
     $http.get('/logeo/sesion')
-      .then(function successCallback(respuesta){
+      .then((respuesta) => {
         loginServicio.logeado = respuesta.data
-        return loginServicio.logeado
       })
 
-  loginServicio.recargarSesion = () => {
+  loginServicio.recargarSesion = () =>
     loginServicio.checkearLogeado()
       .then(function(){
         if(loginServicio.logeado)
           perfilServicio.actualizarPerfil()
       })
 
-  }
-
-  loginServicio.eliminarSesion = () => {
+  loginServicio.eliminarSesion = () =>
     $http.delete('/logeo/sesion')
-      .then(function successCallback(respuesta){
+      .then((respuesta) => {
+        // TODO: Ignorar si da un error
         perfilServicio.perfil = null
         loginServicio.logeado = false
       })
-  }
 
-  angular.element(document).ready(function(){
+  angular.element(document).ready(() => {
     loginServicio.recargarSesion()
   })
 
