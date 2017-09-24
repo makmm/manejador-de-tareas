b5b02fcd9c46422e43acfb4a37da4d952b80048f
makmm <matiartopoulos@gmail.com>
2017-09-24 19:59:39 -0300

    eliminar schema que no se supone que deberia estar acá

--- config/schemas/agenda.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.Promise = global.Promise
-
-let agendaSchema = mongoose.Schema({
-  name: String
-})
-
-module.exports = mongoose.model('Agenda', agendaSchema)
