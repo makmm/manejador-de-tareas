8a14a2d0050855b7d864bdd3015f23fd2fc0474b
makmm <matiartopoulos@gmail.com>
2017-09-25 19:25:50 -0300

    Borrar cosas que no pertenecen

--- config/schemas/homework.js
+++ config/schemas/homework.js
@@ -3,29 +3,18 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
 const Topic = require('./topic.js')
-const Agenda = require('./agenda.js')
-
 let homeworkSchema = mongoose.Schema({
   name: String,
   description: String,
-  topicId: mongoose.Schema.Types.ObjectId,
-  agendaId: mongoose.Schema.Types.ObjectId
+  topicId: mongoose.Schema.Types.ObjectId
 })
 
 homeworkSchema.virtual('topic').get(async function(){
   return await Topic.findById(this.topicId)
 })
 
-homeworkSchema.virtual('agenda').get(async function(){
-  return await Agenda.findById(this.agendaId)
-})
-
 homeworkSchema.query.byTopic = function(topic){
   return this.findOne({topic: topic})
-}
-
-homeworkSchema.query.byAgenda = function(agenda){
-  return this.findOne({agenda: agenda})
 }
 
 module.exports = mongoose.model('Homework', homeworkSchema)
