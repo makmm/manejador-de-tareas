409661237eb1e8b07dd18f01d68f61eb00db384b
makmm <matiartopoulos@gmail.com>
2017-09-26 20:16:57 -0300

    Traducir TabsController

--- public/controllers/tabs.controller.js
+++ public/controllers/tabs.controller.js
@@ -4,23 +4,4 @@ app.controller('TabsController', ($scope, $route, $routeParams, $location) => {
   $scope.$route = $route
   $scope.$location = $location
   $scope.$routeParams = $routeParams
-
-  /*
-  $scope.tab = 0;
-
-  tabsCtrl.setearTab = (tab) => {
-    console.log(tab);
-    $scope.tab = tab;
-  };
-
-  tabsCtrl.tabSeleccionadaEs = (tab) => {
-    return tab == $scope.tab;
-  };
-
-  tabsCtrl.conseguirTab = function(){
-    return $scope.tab;
-  };
-
-  //tabsCtrl.setearTab(1);
-  */
 })
